d9bebc667d124bdbf0d347b016dd77c0995465c7
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-26 13:08:25 +0700

    HWSem7-3

---
+++ Task3/Program.cs
@@ -0,0 +1,60 @@
+﻿/*Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+Console.WriteLine("Let's define 2Darray."); 
+int m = ReadInt("Enter m: ");
+int n = ReadInt("Enter n: ");
+int [,] numbers = new int[m,n];
+
+Console.WriteLine("Array: ");
+
+Create2DArray(numbers);
+Print2DArray(numbers);
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void Create2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+
+{
+    double sum = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        sum += numbers[i,j];
+    }
+    Console.WriteLine($"Arithmetic mean of {j} column => {sum / numbers.GetLength(0)} ");
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
