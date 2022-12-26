ded05fe98682e5d2d344b3ab9f246d5fccfb2e64
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-26 12:28:32 +0700

    HWSem7-1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+
+try 
+{
+    Console.WriteLine("Enter m: ");
+    double m = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter n: ");
+    double n = Convert.ToDouble(Console.ReadLine());
+    double[,] arr = Create2DArray(m,n);
+    Print2DArray(arr);
+}
+catch (Exception)
+{
+    Console.WriteLine("Not correct info!");
+}
+
+void Print2DArray(double [,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] Create2DArray(double m, double n)
+{
+    double[,] array = new double[(int) m, (int) n];
+
+    Random random = new Random();
+
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = Convert.ToDouble (random.Next(-100,100))/10;
+        }
+    }
+
+    return array;
+}
+
+
+    
+
